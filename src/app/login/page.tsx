9ed1884e9a0b93752ca4d7629100b40b1ef566ab
V0yager2 <botLemonTea@gmail.com>
2024-12-15 03:24:58 +0700

    font_login_register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,9 @@ import { useState } from 'react';
 import { login } from '../serverAction/serverAction';
 import { useRouter } from 'next/navigation';
 
-export default function Home(){
+export default function Home() {
     const router = useRouter();
-    
+
     const [errors, setErrors] = useState<{
         email?: string,
         password?: string,
@@ -30,7 +30,7 @@ export default function Home(){
 
     const handleSubmit = async (formData: FormData) => {
         const validationErrors = validateForm(formData);
-        
+
         if (Object.keys(validationErrors).length > 0) {
             setErrors(validationErrors);
             return;
@@ -46,36 +46,40 @@ export default function Home(){
     };
 
     return (
-        <div className='bg-white  h-screen'>
-         <form action={handleSubmit} className="flex flex-col space-y-4 max-w-md mx-auto text-black">
-            <div>
-                <input 
-                    type="email" 
-                    name="email" 
-                    placeholder="Email" 
-                    required 
-                    className="p-2 border rounded w-full"
-                />
-                {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+        <div className='bg-white h-screen flex justify-center items-center'>
+            <div className='w-96 h-auto p-10 shadow-lg rounded-lg ' >
+                <h2 className='text-sm text-center'>Welcome to</h2>
+                <h1 className='text-blue-400 text-4xl font-bold text-center mb-6'>POOJECT</h1>
+                    <form action={handleSubmit} className="flex flex-col space-y-4 max-w-md mx-auto text-black">
+                    <div>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            required
+                            className="p-2 border border-blue-400 rounded w-full"
+                        />
+                        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            required
+                            className="p-2 border border-blue-400 rounded w-full"
+                        />
+                        {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-xl p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                    >
+                        Log in
+                    </button>
+                    <div> Do not have an acount? <a href='register' className='text-blue-400 font-bold'>Register</a></div>
+                </form>
             </div>
-            <div>
-                <input 
-                    type="password" 
-                    name="password" 
-                    placeholder="Password" 
-                    required 
-                    className="p-2 border rounded w-full"
-                />
-                {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
-            </div>
-            <button 
-                type="submit" 
-                className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-                Login
-            </button>
-            <div>Don't have an acount? <a href='register' className='text-blue-500'>register</a></div>
-         </form>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { getCookie } from "./serverAction/serverAction";
 import SideNav from './component/SideNav';
 import Header from './component/Header';
+import Navbar from './component/Navbar';
 
 export default function Home() {
   const router = useRouter();
@@ -26,40 +27,44 @@ export default function Home() {
   }, []);
 
   return (
+    <>
+  <Header/>
     <div className="bg-blue-400 h-screen flex">
-            <SideNav />
-            <div className="flex-1 bg-white shadow-md rounded-l-lg overflow-hidden">
-              <Header />
-                <main className="container mx-auto py-8 grid grid-cols-4 gap-8">
-                <section className="to-do bg-purple-100 p-4 rounded-md">
-                    <h2 className="text-xl font-bold mb-4">TO DO</h2>
-                    <button className="bg-purple-500 text-white px-4 py-2 rounded-md">
-                    Add New Task
-                    </button>
-                </section>
+      <SideNav />
+      <div className="flex-1 bg-white shadow-md rounded-l-lg overflow-hidden">
+        
+        <Navbar />
+        <main className="container mx-auto py-8 grid grid-cols-4 gap-8">
+          <section className="to-do bg-purple-100 p-4 rounded-md">
+            <h2 className="text-xl font-bold mb-4">TO DO</h2>
+            <button className="bg-purple-500 text-white px-4 py-2 rounded-md">
+              Add New Task
+            </button>
+          </section>
 
-                <section className="in-progress bg-blue-100 p-4 rounded-md">
-                    <h2 className="text-xl font-bold mb-4">IN PROGRESS</h2>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                    Add New Task
-                    </button>
-                </section>
+          <section className="in-progress bg-blue-100 p-4 rounded-md">
+            <h2 className="text-xl font-bold mb-4">IN PROGRESS</h2>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              Add New Task
+            </button>
+          </section>
 
-                <section className="testing bg-yellow-100 p-4 rounded-md">
-                    <h2 className="text-xl font-bold mb-4">TESTING</h2>
-                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-md">
-                    Add New Task
-                    </button>
-                </section>
+          <section className="testing bg-yellow-100 p-4 rounded-md">
+            <h2 className="text-xl font-bold mb-4">TESTING</h2>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-md">
+              Add New Task
+            </button>
+          </section>
 
-                <section className="complete bg-green-100 p-4 rounded-md">
-                    <h2 className="text-xl font-bold mb-4">COMPLETE</h2>
-                    <button className="bg-green-500 text-white px-4 py-2 rounded-md">
-                    Add New Task
-                    </button>
-                </section>
-                </main>
-            </div>
-          </div>
+          <section className="complete bg-green-100 p-4 rounded-md">
+            <h2 className="text-xl font-bold mb-4">COMPLETE</h2>
+            <button className="bg-green-500 text-white px-4 py-2 rounded-md">
+              Add New Task
+            </button>
+          </section>
+        </main>
+      </div>
+    </div>
+    </>
   );
 }
